3e980defa3360e974f89faa7af24ee7d9faf8986
yzbbanban <yzbbanban@live.com>
2018-10-04 09:53:43 +0800

    同步锁

--- src/main/java/com/yzb/study/disruptor/detail/Review.java
+++ src/main/java/com/yzb/study/disruptor/detail/Review.java
@@ -3,24 +3,57 @@ package com.yzb.study.disruptor.detail;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by brander on 2018/10/3
  */
 public class Review {
-    public static void main(String[] args) {
-        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+    public static void main(String[] args) throws Exception {
+//        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+//
+//
+//        CopyOnWriteArrayList cowal = new CopyOnWriteArrayList();
+//
+//        cowal.add("11");
+//
+//        AtomicLong count = new AtomicLong(0);
+//
+//        boolean flag = count.compareAndSet(0, 2);
+//        System.out.println(flag);
+//        System.out.println(count.get());
 
 
-        CopyOnWriteArrayList cowal = new CopyOnWriteArrayList();
+        Object lock = new Object();
+        Thread a = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int sum = 0;
+                for (int i = 0; i < 10; i++) {
+                    sum += i;
+                }
+//                synchronized (lock) {
+//                    try {
+//                        lock.wait();
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
 
-        cowal.add("11");
+                LockSupport.park();
 
-        AtomicLong count = new AtomicLong(0);
+                System.out.println(sum);
+            }
+        });
 
-        boolean flag = count.compareAndSet(0, 2);
-        System.out.println(flag);
-        System.out.println(count.get());
+        a.start();
+
+        Thread.sleep(2000);
+
+        LockSupport.unpark(a);
+//        synchronized (lock){
+//            lock.notify();
+//        }
 
     }
 }
